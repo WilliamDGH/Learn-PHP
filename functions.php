5c2e457c2c0c26f932aa309a56a40f69988f29fc
Guanhua Ding <william.ting.1116@gmail.com>
2019-04-25 22:36:24 +1000

    function

--- functions.php
+++ functions.php
@@ -8,11 +8,48 @@
   Pascal Case - MyFunction()
   */
 
+  // Create Simple Function
   function simpleFunction() {
     echo 'Hello World from a function';
     echo '<br>';
   }
 
+  // Run Simple Function
   simpleFunction();
+
+  // Function With Params
+  function sayHello($name = 'World') {
+    echo "Hello $name<br>";
+  }
+
+  sayHello();
+  sayHello('Joe');
+  sayHello('Bob');
+
+  function addNumbers($num1, $num2) {
+    return $num1 + $num2;
+  }
+
+  echo addNumbers(2,3)."<br>";
+
+  // By Reference
+
+  $myNum = 10;
+
+  function addFive($num) {
+    $num += 5;
+  }
+
+  function addTen(&$num) {
+    $num += 5;
+  }
+
+  addFive($myNum);
+
+  echo "Value: $myNum<br>";
+
+  addTen($myNum);
+
+  echo "Value: $myNum<br>";
 
  ?>
